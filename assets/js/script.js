33183ee011a12e0c7426c0201b4db1985deed65f
Michael Rao <jmichaelrao@gmail.com>
2021-05-01 19:28:24 -0500

    add task save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,44 @@
-$(document).ready(function () {
-  // all your code goes here
-  function saveTask() {
-    //get stuff user put in textarea
-    //save that to localstorage
-    //show message to user
-    //hide message after so many seconds
-  }
-  // user clicks save button
-  $(".saveBtn").on("click", saveTask);
-  function updateRowColor() {
-    // set variable to current hour
-    // loop over each row of the class (time-block)
-    // find row time using data attribute (9 < 21)
-    // add the past class to $(this).addClass('past');
-  }
-  // color rows based on time of day
-  // use interval to update row color
+// Define day and hour using Moment
+let today = moment();
+
+// Set date at top of page
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
+function saveTask() {
+  // update row color using below function
   updateRowColor();
-  // load data from local storage and put it in the correct row
-  // #hour9 textarea to value of locaalstorage key number 'hour-9'
-  // #hour10 textarea to value of locaalstorage key number 'hour-10'
-  // display current date on the page - moment (get document by id)
-});
+  // keeps row color up to date every minute
+  setInterval(updateRowColor, 60000);
+  //get stuff user put in textarea
+  $(".saveBtn").on("click", function () {
+    let blockHour = $(this).parent().attr("data-time");
+    let textEntry = $(this).parent().find("textarea").val();
+    //save that to localstorage
+    localStorage.setItem(blockHour, textEntry);
+  });
+
+  //show message to user
+  //hide message after so many seconds
+}
+// user clicks save button
+// $(".saveBtn").on("click", saveTask);
+
+function updateRowColor() {
+  // set variable to current hour
+  let currentHour = moment().hour();
+  // loop over each row of the class (time-block)
+  $(".time-block").each(function () {
+    let blockHour = $(this).attr("data-time");
+    // add classes for color changes
+    if (currentHour > blockHour) {
+      $(this).addClass("past");
+    } else if (currentHour < blockHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+}
+
+// // might wrap app in this function, but we'll see //5/1/21
+// $(document).ready(function () {});
